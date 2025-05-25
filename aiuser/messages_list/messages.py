d1332420d227fe793460b90c3664c4497e52d680
alsaiduq-lab <riiidge.racer@gmail.com>
2025-05-25 17:06:05 -0600

    forgot

--- aiuser/messages_list/messages.py
+++ aiuser/messages_list/messages.py
@@ -69,7 +69,7 @@ class MessagesList:
                 self.model
             )
         else:
-            self.model = await self.config.default_model() or "gpt-4o"
+            self.model = await self.config.default_model()
             self.token_limit = self._get_token_limit(self.model)
         try:
             self._encoding = tiktoken.encoding_for_model(self.model)
